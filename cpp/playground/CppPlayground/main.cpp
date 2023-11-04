b5d4327f658eb70ce03d573fc2f28b60ebcd517b
Timo <timoleitri@aol.com>
2023-11-04 15:47:54 +0100

    Add Visual Studio Project playground

---
+++ cpp/playground/CppPlayground/main.cpp
@@ -0,0 +1,14 @@
+// Exercise 1: Hello, World & Personalization
+// Objective: Compile and run a basic C++ program and personalize the output.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    // put code here
+
+    return 0;
+}
